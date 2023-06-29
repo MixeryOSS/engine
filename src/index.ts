5339acb8be1d56373888d9b73dbb340ff87aedc5
nahkd123 <nahkd123@gmail.com>
2023-06-29 21:00:29 +0700

    Export all components in index.ts

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+export * from "./Workspace.js";
+
+export * from "./addons/Addon.js";
+export * from "./addons/plugins/PluginBase.js";
+export * from "./addons/plugins/PluginGUI.js";
+export * from "./addons/plugins/PluginTab.js";
+export * from "./addons/plugins/PluginType.js";
+export * from "./addons/plugins/effects/Effect.js";
+export * from "./addons/plugins/effects/EffectType.js";
+export * from "./addons/plugins/generators/Generator.js";
+export * from "./addons/plugins/generators/GeneratorType.js";
+
+export * from "./midi/Note.js";
+
+export * from "./projects/Project.js";
+export * from "./projects/ProjectGenerators.js";
+export * from "./projects/Timeline.js";
+export * from "./projects/mixer/Mixer.js";
+export * from "./projects/mixer/MixerTrack.js";
+export * from "./projects/player/Player.js";
+export * from "./projects/playlist/Playlist.js";
+export * from "./projects/playlist/PlaylistTrack.js";
+export * from "./projects/playlist/patterns/MIDIPattern.js";
+export * from "./projects/playlist/patterns/PatternBase.js";
+export * from "./projects/playlist/patterns/PatternHolder.js";
+
+export * from "./utils/Emitter.js";
+export * from "./utils/Label.js";
+export * from "./utils/References.js";
+export * from "./utils/Registry.js";
+export * from "./utils/Temperament.js";
